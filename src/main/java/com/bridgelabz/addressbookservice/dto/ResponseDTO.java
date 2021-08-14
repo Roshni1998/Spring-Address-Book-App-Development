447f2f19edfbc6f755101a7d7f435cb15642202a
Roshni1998 <roshnimali847@gmail.com>
2021-08-15 00:48:59 +0530

    Added: UC-1 AddressBook Model and Response POJO Objects

---
+++ src/main/java/com/bridgelabz/addressbookservice/dto/ResponseDTO.java
@@ -0,0 +1,35 @@
+/* ResponseDTO.java */
+package com.bridgelabz.addressbookservice.dto;
+
+public class ResponseDTO {
+
+    private String message;
+    private Object data;
+
+    // Constructor
+    public ResponseDTO(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    // Getters and Setters
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+
+
+
+}
