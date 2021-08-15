7245ddee21f815de4dadb63f8c3203ac831194f8
Roshni1998 <roshnimali847@gmail.com>
2021-08-15 14:45:39 +0530

    Added: UC-3 Testing the Environments

--- src/main/java/com/bridgelabz/addressbookservice/AddressBookServiceApplication.java
+++ src/main/java/com/bridgelabz/addressbookservice/AddressBookServiceApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbookservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
@@ -10,8 +11,9 @@ public class AddressBookServiceApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(AddressBookServiceApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookServiceApplication.class, args);
 		log.info("Address Book App Started");
+		log.info("AddressBook App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
-
 }
