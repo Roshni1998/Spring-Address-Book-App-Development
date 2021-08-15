fbbd877594ad7e872f94cec0fec1a888e85bcff1
Roshni1998 <roshnimali847@gmail.com>
2021-08-15 11:59:56 +0530

    Added: UC-1 Using Lombok in ResponseDTO

--- src/main/java/com/bridgelabz/addressbookservice/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/addressbookservice/dto/ResponseDTO.java
@@ -1,7 +1,9 @@
 /* ResponseDTO.java */
 package com.bridgelabz.addressbookservice.dto;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public @Data class ResponseDTO {
 
     private String message;
     private Object data;
@@ -11,25 +13,4 @@ public class ResponseDTO {
         this.message = message;
         this.data = data;
     }
-
-    // Getters and Setters
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-
-
-
 }
