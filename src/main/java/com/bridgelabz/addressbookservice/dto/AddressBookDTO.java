fe708beabfec908d9e76d48bfec05a0055a9e32c
Roshni1998 <roshnimali847@gmail.com>
2021-08-16 20:15:50 +0530

    Added: Add validation to various field to validated

--- src/main/java/com/bridgelabz/addressbookservice/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookservice/dto/AddressBookDTO.java
@@ -1,11 +1,19 @@
 /* AddressBookDTO.java */
 package com.bridgelabz.addressbookservice.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class AddressBookDTO {
 
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Invalid First Name")
     public String firstName;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Invalid Last Name")
     public String lastName;
+    @NotEmpty(message = "Address cannot be empty")
     public String address;
+    @Size(max = 10, min = 10, message = "Please Enter a valid mobile number")
     public String mobileNo;
 
     public AddressBookDTO(String firstName, String lastName, String address, String mobileNo) {
