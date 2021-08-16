f3bd5741cad1208c8e424607e0c11037e9f39c32
Roshni1998 <roshnimali847@gmail.com>
2021-08-17 00:23:21 +0530

    Added: UC-2 Provide User Friendly Error Response in case validation fails

---
+++ src/main/java/com/bridgelabz/addressbookservice/exception/AddressBookException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookservice.exception;
+
+public class AddressBookException extends RuntimeException {
+    public AddressBookException(String message) {
+        super(message);
+    }
+}
